2809575829decc19f55dc8079a81952b944236bc
ashroff12 <51920473+ashroff12@users.noreply.github.com>
2020-02-11 19:32:26 -0600

    Week 2

---
+++ Assets/Scripts/CollectSun.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectSun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int score = 100;
+    void OnMouseDown(){
+        score += 25;
+
+        Destroy(gameObject);
+    }
+}
