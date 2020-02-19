8821ee7c91c82dd55aa34751aab4371ed314b2af
ashroff12 <51920473+ashroff12@users.noreply.github.com>
2020-02-18 19:18:51 -0600

    week 3

---
+++ Assets/Scripts/Firing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootingInterval = 0.5f;
+    void Start()
+    {
+        InvokeRepeating("shoot", 0, shootingInterval);
+    }
+
+    void shoot(){
+        if(zombieInFront()){
+            //animation
+            GetComponent<Animator>().SetTrigger("IsFiring");
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+
+    bool zombieInFront(){
+        Vector2 origin = new Vector2(9.5f, transform.position.y);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(origin, -Vector2.right);
+
+        foreach(RaycastHit2D hit in hits){
+            if(hit.collider != null&& hit.collider.gameObject.tag == "zombie"){
+                return true;
+            }
+        }
+        return false;
+    }
+}
