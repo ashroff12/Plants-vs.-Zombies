e7421ead5019a6bddaac83245f3b02e8e31297e8
ashroff12 <51920473+ashroff12@users.noreply.github.com>
2020-02-25 19:27:05 -0600

    week 4

---
+++ Assets/Scripts/BuildMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMenu : MonoBehaviour
+{
+    public Texture sunImage;
+    public BuildInfo[] plants;
+    public static BuildInfo curPlant;
+    void OnGUI(){
+        GUILayout.BeginArea(new Rect(Screen.width/2 - 100, 30, 200, 100));
+        GUILayout.BeginHorizontal("box");
+
+        GUILayout.Box(new GUIContent(CollectSun.score.ToString(), sunImage));
+        foreach(BuildInfo bi in plants){
+            GUI.enabled = CollectSun.score >= bi.price;
+            if(GUILayout.Button(new GUIContent(bi.price.ToString(), bi.previewImage))){
+                curPlant = bi;
+            }
+        }
+
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+}
+}
